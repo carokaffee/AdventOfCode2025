c255aa95e09d1d1a84b44a33f3493ecb3028121a
carokaffee <carolin.kaffine@gmail.com>
2025-12-02 06:30:23 +0100

    day01 refined

--- src/solutions/day01.py
+++ src/solutions/day01.py
@@ -7,23 +7,32 @@ if __name__ == "__main__":
     data = load_data(TESTING, "\n")
     rotations = [(el[0], int(el[1:])) for el in data]
 
-    dial_a = 50
-    dial_b = 50
-    count_a = 0
-    count_b = 0
+    dial_a = dial_b1 = dial_b2 = 50
+    count_a = count_b1 = count_b2 = 0
 
+    # part 1
     for direction, num in rotations:
         step = num if direction == "R" else -num
         dial_a = (dial_a + step) % 100
         if dial_a == 0:
             count_a += 1
 
+    # part 2 (rather dumb but works because input is small enough)
     for direction, num in rotations:
         step = 1 if direction == "R" else -1
         for _ in range(num):
-            dial_b = (dial_b + step) % 100
-            if dial_b == 0:
-                count_b += 1
+            dial_b1 = (dial_b1 + step) % 100
+            if dial_b1 == 0:
+                count_b1 += 1
+
+    # part 2 (more efficient version that wasn't written half asleep at 6am :D)
+    for direction, num in rotations:
+        step = num if direction == "R" else -num
+        count_b2 += abs(dial_b2 + step) // 100
+        if dial_b2 != 0 and dial_b2 + step <= 0:
+            count_b2 += 1
+
+        dial_b2 = (dial_b2 + step) % 100
 
     # PART 1
     # test:      3
@@ -33,4 +42,4 @@ if __name__ == "__main__":
     # PART 2
     # test:      6
     # answer: 6496
-    print(count_b)
+    print(count_b1, count_b2)
