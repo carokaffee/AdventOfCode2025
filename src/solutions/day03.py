27f74b6939d1ddbe90fd2cc695334b0f5890f28e
carokaffee <carolin.kaffine@gmail.com>
2025-12-03 06:32:14 +0100

    day 3 part 1+2

--- src/solutions/day03.py
+++ src/solutions/day03.py
@@ -6,8 +6,8 @@ TESTING = False
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
     batteries = [[int(el) for el in banks] for banks in data]
-    print(batteries)
     values = []
+    voltage = 12
 
     for bank in batteries:
         first = None
@@ -25,5 +25,44 @@ if __name__ == "__main__":
                     second = battery
         values.append(10 * first + second)
 
-    print(values)
     print(sum(values))
+
+    new_values = []
+
+    for bank in batteries:
+        positions = [None] * 12
+        indices = [None] * 12
+        n = len(bank)
+
+        for k in range(9, 0, -1):
+            for i, battery in enumerate(bank[: (-voltage + 1)]):
+                if (positions[0] is None) and battery == k:
+                    positions[0] = battery
+                    indices[0] = i
+
+        for vol in range(1, 11):
+            for k in range(9, 0, -1):
+                for i, battery in enumerate(bank[: (-voltage + vol + 1)]):
+                    if (
+                        battery == k
+                        and (positions[vol - 1] is not None)
+                        and (positions[vol] is None)
+                        and (indices[vol - 1] < i)
+                    ):
+                        positions[vol] = battery
+                        indices[vol] = i
+
+        for k in range(9, 0, -1):
+            for i, battery in enumerate(bank):
+                if (
+                    battery == k
+                    and (positions[voltage - 2] is not None)
+                    and (positions[voltage - 1] is None)
+                    and (indices[voltage - 2] < i)
+                ):
+                    positions[voltage - 1] = battery
+                    indices[voltage - 1] = i
+
+        new_values.append(int("".join([str(el) for el in positions])))
+
+    print(sum(new_values))
