18a7fc078164a4ba2601a3891d1413239d587742
carokaffee <carolin.kaffine@gmail.com>
2025-12-03 09:44:45 +0100

    day 3 refined

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ The entire process of finding, debugging, and refining the solutions involves no
 | :---: | --- | --- |
 | 01 | [Secret Entrance](https://adventofcode.com/2023/day/1) | [day01.py](src/solutions/day01.py) |
 | 02 | [Gift Shop](https://adventofcode.com/2023/day/2) | [day02.py](src/solutions/day02.py) |
-| 03 | [](https://adventofcode.com/2023/day/3) | [](src/solutions/day03.py) |
+| 03 | [Lobby](https://adventofcode.com/2023/day/3) | [day03.py](src/solutions/day03.py) |
 | 04 | [](https://adventofcode.com/2023/day/4) | [](src/solutions/day04.py) |
 | 05 | [](https://adventofcode.com/2023/day/5) | [](src/solutions/day05.py) |
 | 06 | [](https://adventofcode.com/2023/day/6) | [](src/solutions/day06.py) |
--- src/solutions/day03.py
+++ src/solutions/day03.py
@@ -1,36 +1,40 @@
 from src.tools.loader import load_data
 
-TESTING = False
+TESTING = True
 
 
 def find_optimal_batteries(bank, amount):
-    positions = [None] * amount
-    indices = [None] * amount
+    joltage = ""
 
     for idx in range(0, amount):
-        for k in range(9, 0, -1):
-            valid_bank = bank[: (-amount + idx + 1)] if idx != amount - 1 else bank
-            for i, battery in enumerate(valid_bank):
-                if (
-                    battery == k
-                    and (idx == 0 or positions[idx - 1] is not None)
-                    and (positions[idx] is None)
-                    and (idx == 0 or indices[idx - 1] < i)
-                ):
-                    positions[idx] = battery
-                    indices[idx] = i
-    return int("".join([str(el) for el in positions]))
+        valid_bank = bank[: (-amount + idx + 1)] if idx != amount - 1 else bank
+        battery = max(valid_bank)
+        joltage += str(battery)
+        bank = bank[valid_bank.index(battery) + 1 :]
+
+    return int(joltage)
+
+
+def total_output_joltage(banks, amount):
+    total_joltage = 0
+
+    for bank in banks:
+        optimal = find_optimal_batteries(bank, amount)
+        total_joltage += optimal
+
+    return total_joltage
 
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
     banks = [[int(el) for el in banks] for banks in data]
-    amount = 12
 
-    new_values = []
+    # PART 1
+    # test:     357
+    # answer: 17493
+    print(total_output_joltage(banks, 2))
 
-    for bank in banks:
-        optimal = find_optimal_batteries(bank, amount)
-        new_values.append(optimal)
-
-    print(sum(new_values))
+    # PART 2
+    # test:     3121910778619
+    # answer: 173685428989126
+    print(total_output_joltage(banks, 12))
