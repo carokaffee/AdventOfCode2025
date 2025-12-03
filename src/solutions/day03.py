1962c7d715a5d096b6349b3b1e08f350253b147f
carokaffee <carolin.kaffine@gmail.com>
2025-12-03 06:44:03 +0100

    day 3 condensed

--- src/solutions/day03.py
+++ src/solutions/day03.py
@@ -3,66 +3,34 @@ from src.tools.loader import load_data
 TESTING = False
 
 
+def find_optimal_batteries(bank, amount):
+    positions = [None] * amount
+    indices = [None] * amount
+
+    for idx in range(0, amount):
+        for k in range(9, 0, -1):
+            valid_bank = bank[: (-amount + idx + 1)] if idx != amount - 1 else bank
+            for i, battery in enumerate(valid_bank):
+                if (
+                    battery == k
+                    and (idx == 0 or positions[idx - 1] is not None)
+                    and (positions[idx] is None)
+                    and (idx == 0 or indices[idx - 1] < i)
+                ):
+                    positions[idx] = battery
+                    indices[idx] = i
+    return int("".join([str(el) for el in positions]))
+
+
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
-    batteries = [[int(el) for el in banks] for banks in data]
-    values = []
-    voltage = 12
-
-    for bank in batteries:
-        first = None
-        first_index = None
-        second = None
-        n = len(bank)
-        for k in range(9, 0, -1):
-            for i, battery in enumerate(bank[:-1]):
-                if (first is None) and battery == k:
-                    first = battery
-                    first_index = i
-        for k in range(9, 0, -1):
-            for i, battery in enumerate(bank):
-                if (first is not None) and (second is None) and first_index < i and battery == k:
-                    second = battery
-        values.append(10 * first + second)
-
-    print(sum(values))
+    banks = [[int(el) for el in banks] for banks in data]
+    amount = 12
 
     new_values = []
 
-    for bank in batteries:
-        positions = [None] * 12
-        indices = [None] * 12
-        n = len(bank)
-
-        for k in range(9, 0, -1):
-            for i, battery in enumerate(bank[: (-voltage + 1)]):
-                if (positions[0] is None) and battery == k:
-                    positions[0] = battery
-                    indices[0] = i
-
-        for vol in range(1, 11):
-            for k in range(9, 0, -1):
-                for i, battery in enumerate(bank[: (-voltage + vol + 1)]):
-                    if (
-                        battery == k
-                        and (positions[vol - 1] is not None)
-                        and (positions[vol] is None)
-                        and (indices[vol - 1] < i)
-                    ):
-                        positions[vol] = battery
-                        indices[vol] = i
-
-        for k in range(9, 0, -1):
-            for i, battery in enumerate(bank):
-                if (
-                    battery == k
-                    and (positions[voltage - 2] is not None)
-                    and (positions[voltage - 1] is None)
-                    and (indices[voltage - 2] < i)
-                ):
-                    positions[voltage - 1] = battery
-                    indices[voltage - 1] = i
-
-        new_values.append(int("".join([str(el) for el in positions])))
+    for bank in banks:
+        optimal = find_optimal_batteries(bank, amount)
+        new_values.append(optimal)
 
     print(sum(new_values))
