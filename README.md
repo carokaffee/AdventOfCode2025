faff277213ed710a18df74ec3f82fac55e73e41a
carokaffee <carolin.kaffine@gmail.com>
2025-12-04 06:43:02 +0100

    day 4

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # :star: Advent of Code 2025 :star:
 
 My solutions for AoC 2025 written in Python.
+
 The entire process of finding, debugging, and refining the solutions involves no AI of any kind.
 
 ## Overview
@@ -10,7 +11,7 @@ The entire process of finding, debugging, and refining the solutions involves no
 | 01 | [Secret Entrance](https://adventofcode.com/2023/day/1) | [day01.py](src/solutions/day01.py) |
 | 02 | [Gift Shop](https://adventofcode.com/2023/day/2) | [day02.py](src/solutions/day02.py) |
 | 03 | [Lobby](https://adventofcode.com/2023/day/3) | [day03.py](src/solutions/day03.py) |
-| 04 | [](https://adventofcode.com/2023/day/4) | [](src/solutions/day04.py) |
+| 04 | [Printing Department](https://adventofcode.com/2023/day/4) | [day04.py](src/solutions/day04.py) |
 | 05 | [](https://adventofcode.com/2023/day/5) | [](src/solutions/day05.py) |
 | 06 | [](https://adventofcode.com/2023/day/6) | [](src/solutions/day06.py) |
 | 07 | [](https://adventofcode.com/2023/day/7) | [](src/solutions/day07.py) |
--- src/solutions/day04.py
+++ src/solutions/day04.py
@@ -1,7 +1,55 @@
 from src.tools.loader import load_data
 
 TESTING = True
+DIR = tuple((dx, dy) for dx in (-1, 0, 1) for dy in (-1, 0, 1) if dx != 0 or dy != 0)
+
+
+def prepare_grid(data):
+    len_padded = len(data) + 2
+    empty = ["."]
+    rolls = []
+    rolls.append(empty * len_padded)
+    rolls += [empty + [el for el in row] + empty for row in data]
+    rolls.append(["."] * len_padded)
+
+    return rolls
+
+
+def find_accessible_rolls(rolls, iterate=False):
+    num_rows = len(rolls)
+    num_cols = len(rolls[0])
+
+    counter = 0
+    change = True
+
+    while change:
+        change = False
+        for x in range(1, num_rows - 1):
+            for y in range(1, num_cols - 1):
+                if rolls[x][y] == ".":
+                    continue
+                num_near_rolls = 0
+                for dx, dy in DIR:
+                    num_near_rolls += rolls[x + dx][y + dy] == "@"
+                if num_near_rolls < 4:
+                    counter += 1
+                    if iterate:
+                        rolls[x][y] = "."
+                        change = True
+
+    return counter
 
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
+    rolls = prepare_grid(data)
+
+    # PART 1
+    # test:     13
+    # answer: 1370
+    print(find_accessible_rolls(rolls))
+
+    # PART 2
+    # test:     43
+    # answer: 8437
+    print(find_accessible_rolls(rolls, iterate=True))
