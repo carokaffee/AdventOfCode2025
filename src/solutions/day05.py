172794180dfb3ee32d2b17d16b0c1513460efb78
carokaffee <carolin.kaffine@gmail.com>
2025-12-05 06:39:04 +0100

    day 05

--- src/solutions/day05.py
+++ src/solutions/day05.py
@@ -1,7 +1,44 @@
 from src.tools.loader import load_data
+import time
 
-TESTING = True
+TESTING = False
 
 
 if __name__ == "__main__":
-    data = load_data(TESTING, "\n")
+    s = time.time()
+    data = load_data(TESTING, "\n\n")
+
+    ranges = [tuple(map(int, tuple(el.split("-")))) for el in data[0].split("\n")]
+    ingredients = list(map(int, data[1].split("\n")))
+
+    count = 0
+
+    for ing in ingredients:
+        spoiled = 0
+        for start, end in ranges:
+            if start <= ing and ing <= end:
+                spoiled = True
+        count += spoiled
+
+    print(count)
+
+    points = sorted(list(set([point for pair in ranges for point in pair])))
+
+    new_ranges = []
+    for a, b in ranges:
+        special = [num for num in points if a <= num and num <= b]
+        new_ranges += [(num, num) for num in special]
+        for i in range(len(special) - 1):
+            new_ranges.append((special[i] + 1, special[i + 1] - 1))
+
+    new_ranges = set(new_ranges)
+
+    count2 = 0
+    for a, b in new_ranges:
+        if a <= b:
+            count2 += b - a + 1
+
+    print(count2)
+
+    e = time.time()
+    print(e - s)
